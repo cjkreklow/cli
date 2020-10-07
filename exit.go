a17f01e747d39ab0539eb21e045b26e6f3424f05
Collin Kreklow <collin@kreklow.us>
2020-10-07 18:38:56 -0500

    Add ExitHandler
    Separating the exit handling from Cmd, to allow more modular embedding
    of key functions into applications.

--- exit.go
+++ exit.go
@@ -23,86 +23,155 @@
 package cli
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
-// SetExitTimeout sets the length of time Exit() waits before forcing
-// the application to exit. NewCmd sets a default timeout of 5 seconds.
-func (c *Cmd) SetExitTimeout(t time.Duration) {
-	c.exitTimeout.Store(t)
+// ExitHandler provides the ability to gracefully shut down an
+// application, expanding on the functionality of sync.WaitGroup.
+//
+// Calling Exit will close the exit channel C, providing the ability for
+// any goroutines watching C to perform clean up tasks and return.
+//
+// Calling Watch with a list of signals will set up a goroutine to
+// receive those signals and call Exit, allowing for simple trapping of
+// Ctrl-C and kill by passing os.SIGINT and os.SIGTERM.
+//
+// If a timeout has been set, the closure of the exit channel will also
+// trigger a timer which calls os.Exit upon expiration. Sending an exit
+// signal during the timeout will abort the timer and call os.Exit
+// immediately.
+//
+// If an error is passed to Exit, the error will be returned to the
+// caller of Wait once all the goroutines being awaited call Done. If a
+// timeout or signal based forced exit occurs, the error message will be
+// printed to os.Stderr before os.Exit is called.
+//
+type ExitHandler struct {
+	timeout int64 // guarantee 64 bit alignment on 32 bit platforms
+
+	wg sync.WaitGroup
+
+	// C is the exit channel. Must call Add or Watch before attempting
+	// to receive from C.
+	C <-chan bool
+
+	// ec is the send end of C, reserved for closing.
+	ec chan<- bool
+
+	sc chan os.Signal
+
+	exitOnce  sync.Once
+	watchOnce sync.Once
+
+	err error
 }
 
-// Exit gracefully closes the application by closing the exit channel
-// and waiting for pending operations to finish. The error value of the
-// first call to Exit() will be passed to the caller of Wait(). In order
-// to be successful, the calling application must make use of
-// ExitChannel and AddWait.
-func (c *Cmd) Exit(err error) {
-	c.exitOnce.Do(func() {
-		c.err = err
+// SetTimeout sets the timeout duration. A zero or negative value waits
+// indefinitely.
+func (e *ExitHandler) SetTimeout(t time.Duration) {
+	atomic.StoreInt64(&e.timeout, int64(t))
+}
 
-		close(c.exitChan)
+// Exit closes the exit channel and starts the timeout timer, if
+// applicable. The error value passed to the first Exit call will be
+// passed as the return value of Wait. Exit is safe to call multiple
+// times, all calls after the first are ignored.
+func (e *ExitHandler) Exit(err error) {
+	e.exitOnce.Do(func() {
+		e.err = err
 
-		go func() {
-			<-time.After(c.exitTimeout.Load().(time.Duration))
+		close(e.ec)
 
-			c.Eprintln("exit forced by timeout")
+		t := atomic.LoadInt64(&e.timeout)
 
-			if c.err != nil {
-				c.Eprintln(c.err)
-			}
-
-			os.Exit(1)
-		}()
+		if t > 0 {
+			go e.timeoutWait(t)
+		}
 	})
 }
 
-// ExitChannel returns a read-only channel that will be closed when the
-// application is exiting.
-func (c *Cmd) ExitChannel() <-chan bool {
-	return c.exitChan
-}
-
-// AddWait notifies Cmd that a goroutine is running and Wait() should
-// not return until the goroutine is finished. The goroutine must call
-// Done() before exiting.
-func (c *Cmd) AddWait() {
-	c.exitWg.Add(1)
-}
-
-// Done notifies the Cmd that a goroutine previously added with
-// AddWait() is now complete.
-func (c *Cmd) Done() {
-	c.exitWg.Done()
-}
-
-// Wait blocks until all goroutines added with AddWait() have called
-// Done(). The return value will be any error set by the first call to
-// Exit(). Wait() should typically be called at the end of main() to
-// avoid exiting prematurely.
-func (c *Cmd) Wait() error {
-	c.exitWg.Wait()
-
-	return c.err
-}
-
-// watchExitSignal is an internal function to watch for common keyboard
-// interrupt signals and gracefully exit the application.
-func (c *Cmd) watchExitSignal(sigChan <-chan os.Signal) {
+// timeoutWait implements the timeout, called once by Exit.
+func (e *ExitHandler) timeoutWait(t int64) {
 	select {
-	case <-sigChan:
-	case <-c.exitChan:
+	case <-time.After(time.Duration(t)):
+		fmt.Fprintln(os.Stderr, "exit forced by timeout")
+	case <-e.sc:
+		fmt.Fprintln(os.Stderr, "exit forced by signal")
 	}
 
-	c.Exit(nil)
-
-	<-sigChan
-	c.Eprintln("exit forced by signal")
-
-	if c.err != nil {
-		c.Eprintln(c.err)
+	if e.err != nil {
+		fmt.Fprintln(os.Stderr, e.err)
 	}
 
-	os.Exit(1)
+	os.Exit(int(syscall.ETIME))
+}
+
+// Add updates the WaitGroup counter, adding or subtracting as
+// appropriate. Add will panic if the counter goes negative.
+//
+// Add also initializes exit channel C if it has not been initialized
+// previously.
+func (e *ExitHandler) Add(n int) {
+	if e.ec == nil {
+		c := make(chan bool)
+		e.C = c
+		e.ec = c
+	}
+
+	e.wg.Add(n)
+}
+
+// Done removes one from the WaitGroup counter.
+func (e *ExitHandler) Done() {
+	e.wg.Done()
+}
+
+// Wait blocks until the WaitGroup counter is zero. The return value is
+// the first error value passed to Exit.
+func (e *ExitHandler) Wait() error {
+	e.wg.Wait()
+
+	return e.err
+}
+
+// Watch takes a list of signals to receive from the operating system
+// which will trigger Exit. Watch can be called multiple times, each
+// call to Watch will replace the previous list of signals with the new
+// list. An empty list will stop receiving signals from the OS.
+func (e *ExitHandler) Watch(signals ...os.Signal) {
+	if e.sc == nil {
+		e.sc = make(chan os.Signal, 1)
+	}
+
+	signal.Stop(e.sc)
+
+	if len(signals) == 0 {
+		return
+	}
+
+	signal.Notify(e.sc, signals...)
+
+	e.watchOnce.Do(func() {
+		if e.ec == nil {
+			c := make(chan bool)
+			e.C = c
+			e.ec = c
+		}
+
+		go func() {
+			select {
+			case <-e.sc:
+			case <-e.C:
+				return
+			}
+
+			e.Exit(nil)
+		}()
+	})
 }
